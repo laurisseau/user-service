4e7dfc04ba244f1a055e33cf35533936fd078141
laurisseau Joseph <reso0208@gmail.com>
2025-08-19 21:50:17 -0400

    commit

--- handler/middleware/middleware.go
+++ handler/middleware/middleware.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -9,31 +8,12 @@ import (
 
 // IsAuthenticated is a middleware that checks if
 // the user has already been authenticated previously.
-/*
+
 func IsAuthenticated(ctx *gin.Context) {
-	fmt.Println("auth method")
 	if sessions.Default(ctx).Get("profile") == nil {
 		ctx.Redirect(http.StatusSeeOther, "/")
 	} else {
 		ctx.Next()
 	}
 }
-*/
 
-	func IsAuthenticated(ctx *gin.Context) {
-    fmt.Println("auth method")
-    session := sessions.Default(ctx)
-
-    if session.Get("profile") == nil {
-        // For APIs: send 401
-        ctx.AbortWithStatus(http.StatusUnauthorized)
-        return
-
-        // For browser routes: redirect + abort
-        // ctx.Redirect(http.StatusSeeOther, "/")
-        // ctx.Abort()
-        // return
-    }
-
-    ctx.Next()
-}
